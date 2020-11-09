3830bdbeea1dc2582e40d4d37b584b2b93112a43
Rafal Warda <rwarda00@gmail.com>
2020-11-09 01:26:43 +0100

    Fill searchbar template

--- movies/src/components/SearchBar.vue
+++ movies/src/components/SearchBar.vue
@@ -1,10 +1,38 @@
 <template>
+  <div>
+    <form>
+      <div class="form-group">
+          <label for=inputTitle>Tytuł</label>
+          <input type="text" id=inputTitle class="form-control" placeholder="Podaj tytuł lub fragment tytułu filmu"/>
+      </div>
+      <div class="form-group row">
+        <label class="col-sm-4 col-form-label" for="inputProductionFrom">Rok produkcji od:</label>
+        <div class="col-sm-8">
+            <input type="text" id=inputProductionFrom class="form-control"  placeholder="Liczba naturalna z przedziału 1900-2019"/>
+        </div>
+      </div>
+      <div class="form-group row">
+          <label class="col-sm-4 col-form-label" for="inputProductionTo">Rok produkcji do:</label>
+          <div class="col-sm-8">
+              <input type="text" id=inputProductionTo class="form-control" placeholder="Liczba naturalna z przedziału 1900-2019"/>
+          </div>
+      </div>
+      <div class="form-group">
+        <label for="inputCast">Obsada</label>
+        <input type="text" id="inputCast" class="form-control" placeholder="Imię i nazwisko"/>
+      </div>
+      <div class="form-group row">
+          <input type="button" class="btn btn-info col-sm-12" value="Szukaj"/>
+      </div>
+    </form>
+  </div>
 </template>
 
 <script>
 export default {
   name: 'SearchBar',
   props: {
+    movies: Array
   }
 }
 </script>
